c081f1521cbf046c49e34577cd9affe3cdaaea11
mahmoudfiron <mahmoudfiron13@gmail.com>
2025-03-08 05:07:07 +0200

    Yoga ai trainer home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,35 +6,17 @@ import '../styles/Home.css';
 export default function Home() {
 
     return (
-        <div className='home-container'>
-            <div className='home-header'>
-                <h1 className='home-heading'>YogaIntelliJ</h1>
-                <Link to='/about'>
-                    <button 
-                        className="btn btn-secondary" 
-                        id="about-btn"
-                    >
-                        About
-                    </button>
-                </Link>
-            </div>
 
-            <h1 className="description">A Yoga AI Trainer</h1>
             <div className="home-main">
+                <h1 className="title">Yoga AI Trainer</h1>
                 <div className="btn-section">
                     <Link to='/ai-trainer/yoga'>
                         <button
                             className="btn start-btn"
                         >Let's Start</button>
                     </Link>
-                    <Link to='/tutorials'>
-                        <button
-                            className="btn start-btn"
-                        >Tutorials</button>
-                    </Link>
 
                 </div>
             </div>
-        </div>
     )
 }
